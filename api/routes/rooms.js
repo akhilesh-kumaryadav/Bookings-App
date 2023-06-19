7cf83fc29e48a27ec324810e5565fd12c225e22d
akhilesh-kumar-yadav <96679526+akhilesh-kumaryadav@users.noreply.github.com>
2023-06-19 14:12:28 +0530

    backend done

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,7 +1,29 @@
 import express from "express";
 
+import { verifyAdmin } from "../utils/verifyToken.js";
+import {
+  createRoom,
+  updateRoom,
+  deleteRoom,
+  getRoom,
+  getAllRooms,
+} from "../controllers/room.js";
+
 const router = express.Router();
 
+//CREATE
+router.post("/:hotelid", verifyAdmin, createRoom);
 
+//UPDATE
+router.put("/:id", verifyAdmin, updateRoom);
+
+//DELETE
+router.delete("/:id/:hotelid", verifyAdmin, deleteRoom);
+
+//GET
+router.get("/:id", getRoom);
+
+//GET ALL
+router.get("/", getAllRooms);
 
 export default router;
