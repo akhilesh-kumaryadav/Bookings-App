36cf5d14359be6bad6fdb76d9c8fdad3134e5102
akhilesh-kumar-yadav <96679526+akhilesh-kumaryadav@users.noreply.github.com>
2023-06-20 10:20:10 +0530

    done till 14426

--- api/controllers/hotel.js
+++ api/controllers/hotel.js
@@ -58,15 +58,16 @@ export const getAllHotel = async (req, res, next) => {
   }
 };
 
-
 //GET ALL count by city
 export const countByCity = async (req, res, next) => {
   const cities = req.query.cities.split(",");
 
   try {
-    const list = await Promise.all(cities.map(city=>{
-      return Hotel.countDocuments({city:city})
-    }))
+    const list = await Promise.all(
+      cities.map((city) => {
+        return Hotel.countDocuments({ city: city });
+      })
+    );
     res.status(200).json(list);
   } catch (err) {
     next(err);
